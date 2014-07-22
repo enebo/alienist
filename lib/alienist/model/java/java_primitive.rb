9ce69b734a69e9023e186cb3e46ace8c9add4441
Thomas E. Enebo <tom.enebo@gmail.com>
2014-07-22 16:11:31 -0500

    Add proper type labels for primitives and also just display value + type label in inspect

--- lib/alienist/model/java/java_primitive.rb
+++ lib/alienist/model/java/java_primitive.rb
@@ -4,8 +4,12 @@ module Alienist
       class JavaPrimitive
         attr_reader :value
         
-        def initialize(value)
-          @value = value
+        def initialize(value, type_label)
+          @value, @type_label = value, type_label
+        end
+
+        def inspect
+          "#{@type_label}: #{@value.to_s}"
         end
       end
     end
--- lib/alienist/model/java/java_primitives.rb
+++ lib/alienist/model/java/java_primitives.rb
@@ -6,11 +6,11 @@ module Alienist
       ##
       # All primitives read in a single value and are identical
       # in how they are constructed.
-      def self.java_primitive(class_name, reader_method_name)
+      def self.java_primitive(class_name, reader_method_name, type_label)
         class_eval <<-EOS
           class #{class_name} < Alienist::Model::Java::JavaPrimitive
             def self.create(reader)
-              new parse(reader)
+              new parse(reader), '#{type_label}'
             end
 
             def self.parse(reader)
@@ -20,15 +20,15 @@ module Alienist
         EOS
       end
 
-      java_primitive(:JavaBoolean, :read_boolean)
-      java_primitive(:JavaByte, :read_byte)
-      java_primitive(:JavaChar, :read_char)
-      java_primitive(:JavaDouble, :read_double)
-      java_primitive(:JavaFloat, :read_float)
-      java_primitive(:JavaInt, :read_int)
-      java_primitive(:JavaLong, :read_long)
-      java_primitive(:JavaObjectRef, :read_id) # Close enough to primitive
-      java_primitive(:JavaShort, :read_short)
+      java_primitive(:JavaBoolean, :read_boolean, 'boolean')
+      java_primitive(:JavaByte, :read_byte, 'byte')
+      java_primitive(:JavaChar, :read_char, 'char')
+      java_primitive(:JavaDouble, :read_double, 'double')
+      java_primitive(:JavaFloat, :read_float, 'float')
+      java_primitive(:JavaInt, :read_int, 'int')
+      java_primitive(:JavaLong, :read_long, 'long')
+      java_primitive(:JavaObjectRef, :read_id, 'ref') # Close enough to prim.
+      java_primitive(:JavaShort, :read_short, 'short')
     end
   end
 end
